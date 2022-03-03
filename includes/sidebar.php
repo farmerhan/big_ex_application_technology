de65566339b982d3a98b47ecc780fc052ff59628
40_1951060855_Triệu Tiến Mạnh <95534626+TrieuTienManh@users.noreply.github.com>
2022-03-03 18:57:33 +0700

    Sửa thanh sidebar dưới cùng

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -270,59 +270,82 @@
 
 
 
+<!--- panel panel-default sidebar-menu Starts -->
 <div class="panel panel-default sidebar-menu">
     <!--- panel panel-default sidebar-menu Starts -->
-
     <div class="panel-heading">
         <!-- panel-heading Starts -->
-
         <h3 class="panel-title">
             <!-- panel-title Starts -->
-
             Lựa chọn để tìm kiếm
-
             <div class="pull-right">
                 <!-- pull-right Starts -->
-
                 <a href="#" style="color:black;">
-
                     <span class="nav-toggle hide-show">
-
                         Ẩn bớt
-
                     </span>
-
                 </a>
-
             </div><!-- pull-right Ends -->
-
-
         </h3><!-- panel-title Ends -->
-
     </div><!-- panel-heading Ends -->
-
     <div class="panel-collapse collapse-data">
         <!-- panel-collapse collapse-data Starts -->
-
         <div class="panel-body">
             <!-- panel-body Starts -->
-
             <div class="input-group">
                 <!-- input-group Starts -->
-
                 <input type="text" class="form-control" id="dev-table-filter" data-action="filter" data-filters="#dev-cats" placeholder="Nhập vào tên sản phẩm">
-
                 <a class="input-group-addon"> <i class="fa fa-search"> </i> </a>
-
             </div><!-- input-group Ends -->
-
         </div><!-- panel-body Ends -->
-
         <div class="panel-body scroll-menu">
             <!-- panel-body scroll-menu Starts -->
-
             <ul class="nav nav-pills nav-stacked category-menu" id="dev-cats">
                 <!-- nav nav-pills nav-stacked category-menu Starts -->
+                <li class="checkbox checkbox-primary" style="background:#dddddd;">
+                    <a>
+                        <label>
+                            <input type="checkbox" value="4" name="cat" class="get_cat" id="cat">
+                            <span>
+                                <img src="admin_area/other_images/othericon.png" width="20">&nbsp;
+                                Others
+                            </span>
+                        </label>
+                    </a>
+                </li>
+                <li class="checkbox checkbox-primary" style="background:#dddddd;">
+                    <a>
+                        <label>
+                            <input type="checkbox" value="5" name="cat" class="get_cat" id="cat">
+                            <span>
+                                <img src="admin_area/other_images/malelg.png" width="20">&nbsp;
+                                Men
+                            </span>
+                        </label>
+                    </a>
+                </li>
+                <li class="checkbox checkbox-primary">
+                    <a>
+                        <label>
+                            <input type="checkbox" value="2" name="cat" class="get_cat" id="cat">
+                            <span>
+                                <img src="admin_area/other_images/feminelg.png" width="20">&nbsp;
+                                Feminine
+                            </span>
+                        </label>
+                    </a>
+                </li>
+                <li class="checkbox checkbox-primary">
+                    <a>
+                        <label>
+                            <input type="checkbox" value="3" name="cat" class="get_cat" id="cat">
+                            <span>
+                                <img src="admin_area/other_images/kidslg.jpg" width="20">&nbsp;
+                                Kids
+                            </span>
+                        </label>
+                    </a>
+                </li>
             </ul><!-- nav nav-pills nav-stacked category-menu Ends -->
 
         </div><!-- panel-body scroll-menu Ends -->
