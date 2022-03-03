ba32d5985fae26ffc229d9d5e56742498b6bfcf4
BufiQuana <92876948+BufiQuana@users.noreply.github.com>
2022-03-03 20:38:06 +0700

    update customer_login

--- customer/customer_login.php
+++ customer/customer_login.php
@@ -1,122 +1,104 @@
-<div class="box" ><!-- box Starts -->
+<div class="box">
+    <!-- box Starts -->
 
-<div class="box-header" ><!-- box-header Starts -->
+    <div class="box-header">
+        <!-- box-header Starts -->
 
-<center>
+        <center>
 
-<h1>Login</h1>
+            <h1>Đăng nhập tài khoản</h1>
 
-<p class="lead" >Already our Customer</p>
+        </center>
 
+    </div><!-- box-header Ends -->
+    <form action="checkout.php" method="post">
+        <!--form Starts -->
+        <div class="form-group form-signin">
+            <!-- form-group Starts -->
 
-</center>
+            <label>Email: </label>
 
-<p class="text-muted" >
-Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
+            <input type="text" class="form-control" name="c_email" required>
 
-</p>
+        </div><!-- form-group Ends -->
 
+        <div class="form-group form-signin">
+            <!-- form-group Starts -->
 
+            <label>Mật khẩu: </label>
 
+            <input type="password" class="form-control" name="c_pass" required>
 
-</div><!-- box-header Ends -->
+        </div><!-- form-group Ends -->
+        <div class="text-center">
+            <!-- text-center Starts -->
 
-<form action="checkout.php" method="post" ><!--form Starts -->
+            <button name="login" value="Login" class="btn btn-primary">
 
-<div class="form-group" ><!-- form-group Starts -->
-
-<label>Email</label>
-
-<input type="text" class="form-control" name="c_email" required >
-
-</div><!-- form-group Ends -->
-
-<div class="form-group" ><!-- form-group Starts -->
-
-<label>Password</label>
-
-<input type="password" class="form-control" name="c_pass" required >
-
-</div><!-- form-group Ends -->
-
-<div class="text-center" ><!-- text-center Starts -->
-
-<button name="login" value="Login" class="btn btn-primary" >
-
-<i class="fa fa-sign-in" ></i> Log in
+                <i class="fa fa-sign-in"></i> Đăng nhập
 
 
-</button>
+            </button>
 
-</div><!-- text-center Ends -->
-
-
-</form><!--form Ends -->
-
-<center><!-- center Starts -->
-
-<a href="customer_register.php" >
-
-<a href="./customer_register.php" style="display: block; margin-top: 10px; text-decoration: underline;">Đăng kí tài khoản</a>
-
-</a>
+        </div><!-- text-center Ends -->
 
 
-</center><!-- center Ends -->
+    </form>
+    <!--form Ends -->
+
+    <center>
+        <!-- center Starts -->
+        <h3>Chưa có tài khoản? <span><a href="customer_register.php">Đăng kí ở đây</a></span></h3>
+
+    </center><!-- center Ends -->
 
 
 </div><!-- box Ends -->
 
 <?php
 
-if(isset($_POST['login'])){
+if (isset($_POST['login'])) {
 
-$customer_email = $_POST['c_email'];
+    $customer_email = $_POST['c_email'];
 
-$customer_pass = $_POST['c_pass'];
+    $customer_pass = $_POST['c_pass'];
 
-$select_customer = "select * from customers where customer_email='$customer_email' AND customer_pass='$customer_pass'";
+    $select_customer = "select * from customers where customer_email='$customer_email' AND customer_pass='$customer_pass'";
 
-$run_customer = mysqli_query($con,$select_customer);
+    $run_customer = mysqli_query($con, $select_customer);
 
-$get_ip = getRealUserIp();
+    $get_ip = getRealUserIp();
 
-$check_customer = mysqli_num_rows($run_customer);
+    $check_customer = mysqli_num_rows($run_customer);
 
-$select_cart = "select * from cart where ip_add='$get_ip'";
+    $select_cart = "select * from cart where ip_add='$get_ip'";
 
-$run_cart = mysqli_query($con,$select_cart);
+    $run_cart = mysqli_query($con, $select_cart);
 
-$check_cart = mysqli_num_rows($run_cart);
+    $check_cart = mysqli_num_rows($run_cart);
 
-if($check_customer==0){
+    if ($check_customer == 0) {
 
-echo "<script>alert('password or email is wrong')</script>";
+        echo "<script>alert('password or email is wrong')</script>";
 
-exit();
+        exit();
+    }
 
-}
+    if ($check_customer == 1 and $check_cart == 0) {
 
-if($check_customer==1 AND $check_cart==0){
+        $_SESSION['customer_email'] = $customer_email;
 
-$_SESSION['customer_email']=$customer_email;
+        echo "<script>alert('You are Logged In')</script>";
 
-echo "<script>alert('You are Logged In')</script>";
+        echo "<script>window.open('customer/my_account.php?my_orders','_self')</script>";
+    } else {
 
-echo "<script>window.open('customer/my_account.php?my_orders','_self')</script>";
+        $_SESSION['customer_email'] = $customer_email;
 
-}
-else {
+        echo "<script>alert('You are Logged In')</script>";
 
-$_SESSION['customer_email']=$customer_email;
-
-echo "<script>alert('You are Logged In')</script>";
-
-echo "<script>window.open('checkout.php','_self')</script>";
-
-} 
-
-
+        echo "<script>window.open('checkout.php','_self')</script>";
+    }
 }
 
 ?>
