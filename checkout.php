1f06137a00764fbfa47598f93aee39c70aa7a411
BufiQuana <92876948+BufiQuana@users.noreply.github.com>
2022-03-03 20:37:08 +0700

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -14,7 +14,7 @@ include("includes/main.php");
   <!-- HERO -->
   <div class="nero">
     <div class="nero__heading">
-      <span class="nero__bold">Checkout</span>
+      <span class="nero__bold">Đăng nhập</span>
     </div>
     <p class="nero__text">
     </p>
