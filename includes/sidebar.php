a9319c08442e373cb2469b45f294b31b6416531d
40_1951060855_Triệu Tiến Mạnh <95534626+TrieuTienManh@users.noreply.github.com>
2022-03-03 08:46:19 +0700

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -83,12 +83,10 @@ if (isset($_REQUEST['cat']) && is_array($_REQUEST['cat'])) {
     <form class="panel-collapse collapse-data" action= "load.php" method= "POST">
         <!-- panel-collapse collapse-data Starts -->
 
-        <div class="panel-body">
+        <div class="panel-body"  style="display:flex;">
             <!-- panel-body Starts -->
-
                 <input id="input_search" type="text" class="form-control" name="txtTimKiem" id="dev-table-filter" data-action="filter" data-filters="#dev-p-cats" placeholder="Nhập vào tên sản phẩm">
-
-                <button class="button_control_panel" name="btnTimKiem" type="submit" style="border: 0;"><i class="fa fa-search"></i> </button>
+                <button class="button_control_panel" name="btnTimKiem" type="submit" style="border: 0; padding: 5px 12px; background:#dddddd;"><i class="fa fa-search"></i> </button>
 
         </div><!-- panel-body Ends -->
 
@@ -141,110 +139,6 @@ if (isset($_REQUEST['cat']) && is_array($_REQUEST['cat'])) {
                     </li>
                 </div>
 
-                <?php
-                
-
-                // $get_p_cats = "select * from product_categories where p_cat_top='yes'";
-
-                // $run_p_cats = mysqli_query($con, $get_p_cats);
-
-                // while ($row_p_cats = mysqli_fetch_array($run_p_cats)) {
-
-                //     $p_cat_id = $row_p_cats['p_cat_id'];
-
-                //     $p_cat_title = $row_p_cats['p_cat_title'];
-
-                //     $p_cat_image = $row_p_cats['p_cat_image'];
-
-                //     if ($p_cat_image == "") {
-                //     } else {
-
-                //         $p_cat_image = "<img src='admin_area/other_images/$p_cat_image' width='20'> &nbsp;";
-                //     }
-
-                //     echo "
-
-                // <li class='checkbox checkbox-primary' style='background:#dddddd;' >
-
-                // <a>
-
-                // <label>
-
-                // <input ";
-
-                //                     if (isset($aPCat[$p_cat_id])) {
-                //                         echo "checked='checked'";
-                //                     }
-
-                //                     echo " type='checkbox' value='$p_cat_id' name='p_cat' class='get_p_cat' id='p_cat' >
-
-                // <span>
-
-                // $p_cat_image
-                // $p_cat_title
-
-                // </span>
-
-                // </label>
-
-                // </a>
-
-                // </li>
-
-                // ";
-                // }
-
-                // $get_p_cats = "select * from product_categories where p_cat_top='no'";
-
-                // $run_p_cats = mysqli_query($con, $get_p_cats);
-
-                // while ($row_p_cats = mysqli_fetch_array($run_p_cats)) {
-
-                //     $p_cat_id = $row_p_cats['p_cat_id'];
-
-                //     $p_cat_title = $row_p_cats['p_cat_title'];
-
-                //     $p_cat_image = $row_p_cats['p_cat_image'];
-
-                //     if ($p_cat_image == "") {
-                //     } else {
-
-                //         $p_cat_image = "<img src='admin_area/other_images/$p_cat_image' width='20'> &nbsp;";
-                //     }
-
-                //     echo "
-
-                // <li class='checkbox checkbox-primary'>
-
-                // <a>
-
-                // <label>
-
-                // <input ";
-
-                //                     if (isset($aPCat[$p_cat_id])) {
-                //                         echo "checked='checked'";
-                //                     }
-
-                //                     echo " type='checkbox' value='$p_cat_id' name='p_cat' class='get_p_cat' id='p_cat' >
-
-                // <span>
-
-                // $p_cat_image
-                // $p_cat_title
-
-                // </span>
-
-                // </label>
-
-                // </a>
-
-                // </li>
-
-                // ";
-                // }
-
-                ?>
 
             </ul><!-- nav nav-pills nav-stacked category-menu Ends -->
 
