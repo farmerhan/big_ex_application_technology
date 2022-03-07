beee26f194f7bf58d19488e5aba31db0334143e7
shiba20212021 <shiba2021shiba2021@gmail.com>
2022-03-07 20:59:46 +0700

    update the final change pass

--- customer/change_pass.php
+++ customer/change_pass.php
@@ -3,41 +3,41 @@
 
 <form action="" method="post"><!-- form Starts -->
 
-<div class="form-group"><!-- form-group Starts -->
+    <div class="form-group"><!-- form-group Starts -->
 
-<label>Enter Your Current Password</label>
+        <label>Enter Your Current Password</label>
 
-<input type="text" name="old_pass" class="form-control" required>
+        <input type="text" name="old_pass" class="form-control" required>
 
-</div><!-- form-group Ends -->
-
-
-<div class="form-group"><!-- form-group Starts -->
-
-<label>Enter Your New Password</label>
-
-<input type="text" name="new_pass" class="form-control" required>
-
-</div><!-- form-group Ends -->
+    </div><!-- form-group Ends -->
 
 
-<div class="form-group"><!-- form-group Starts -->
+    <div class="form-group"><!-- form-group Starts -->
 
-<label>Enter Your New Password Again</label>
+        <label>Enter Your New Password</label>
 
-<input type="text" name="new_pass_again" class="form-control" required>
+        <input type="text" name="new_pass" class="form-control" required>
 
-</div><!-- form-group Ends -->
+    </div><!-- form-group Ends -->
 
-<div class="text-center"><!-- text-center Starts -->
 
-<button type="submit" name="submit" class="btn btn-primary">
+    <div class="form-group"><!-- form-group Starts -->
 
-<i class="fa fa-user-md"> </i> Change Password
+        <label>Enter Your New Password Again</label>
 
-</button>
+        <input type="text" name="new_pass_again" class="form-control" required>
 
-</div><!-- text-center Ends -->
+    </div><!-- form-group Ends -->
+
+    <div class="text-center"><!-- text-center Starts -->
+
+        <button type="submit" name="submit" class="btn btn-primary">
+
+            <i class="fa fa-user-md"> </i> Change Password
+
+        </button>
+
+    </div><!-- text-center Ends -->
 
 </form><!-- form Ends -->
 <?php
